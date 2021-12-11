56b19e083647f2477174e47662957b9974051557
dtbj <87176273+dtbj@users.noreply.github.com>
2021-12-11 21:16:03 +0530

    Update HomepageFeatures.js

--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -4,12 +4,11 @@ import styles from './HomepageFeatures.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
+    title: 'Content from INBO Toppers',
     Svg: require('../../static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+  Directy learn from INBO toppers and learn new concepts at first sight.
       </>
     ),
   },
@@ -18,18 +17,16 @@ const FeatureList = [
     Svg: require('../../static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        No ADS! No Trackers! Learn without any disturbance only on TalentBrick.
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'Quality and Specific Content',
     Svg: require('../../static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+      "Don't study hard but study Smart!" We give you specific guidance for each topic from different authors.
       </>
     ),
   },
