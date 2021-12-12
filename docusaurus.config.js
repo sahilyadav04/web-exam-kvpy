2cba31b44c363a96f0f9c2206974d8fe35199f5e
dtbj <gs5.gstores@gmail.com>
2021-12-12 18:47:05 +0530

    CSS Optimisation.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -66,8 +66,9 @@ const config = {
           {to: '/blog', label: 'Blog', position: 'left'},
           {
             href: 'https://github.com/talentbrick/web-exam-inbo',
-            label: 'GitHub',
             position: 'right',
+            className: 'header-github-link',
+            'aria-label': 'GitHub Repository',
           },
         ],
       },
